0e0b587e236a3f4687c6850c5245757242ec79d8
olik21 <kolga210921@gmail.com>
2018-03-14 00:40:04 +0200

    Script preload

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    
     <link href="css/style.css" rel="stylesheet">
+
+    <script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
 </head>
 <body>
     <header>
@@ -20,25 +24,13 @@
             <h2 class="visually-hidden">Items</h2>
             <ul class="catalog-list">
 
+
         
-                <?php foreach (getItems(1, 4) as $item): ?>
-                <li class="catalog-item">
-                    <img src="<?php echo $item['img']; ?>" alt="<?php echo $item['title']; ?>">
-                    <h3><?= $item['title']; ?></h3>
-                    <?= $item['description']; ?>
-                    <?php echo $item['discountCost'] ? $item['discountCost'] : $item['cost']; ?>
-                    <?php if ($item['discountCost'] !== null): ?>
-                        <span><?= $item['cost']; ?>
-                         Sale</span>
-                    <?php endif; ?>
-                    <?php if ($item['new']): ?>
-                        <span>New</span>
-                    <?php endif; ?>
-                </li>
-                <?php endforeach; ?>
+
             </ul>
             
-            <button>Load more</button>
+            <button id="show_more" onclick="ShowItems(true)">Load more</button>
+             <div class="loader"></div> 
         </section>
     </main>
     
